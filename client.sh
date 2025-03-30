e1a085385ed6fbb80ab7380d2683d26bf5891f9f
Gohan-ssj2 <david.alonso@enti.cat>
2025-03-30 23:21:02 +0200

    acabado
    al fin copon

--- client.sh
+++ client.sh
@@ -1,78 +1,109 @@
 #!/bim/bash
 
-if [ $# -ne 1 ]
-then
-	echo "Error: El comando requiere al menos un parametro"
-	echo "Ejemplo de uso: "
-	echo -e "\t$0 127.0.0.1"
-	exit 100
-fi
+ if [ $# -ne 1 ]
+ then
+     echo "Error: El comando requiere al menos un parametro"
+     echo "Ejemplo de uso: "
+     echo -e "\t$0 127.0.0.1"
+     exit 100
+ fi
 
-PORT=7777
+ PORT=7777
 
-IP_SERVER=$1
+ IP_SERVER=$1
 
-IP_PROPIA="`ip a | grep -w -i inet | grep -i enp0s3 | cut -d "i" -f 2 | cut -d " " -f 2`"
+ IP_PROPIA="`ip a | grep -w -i inet | grep -i enp0s3 | cut -d "i" -f 2 | cut -d " " -f 2`"
 
 
-echo "$IP_PROPIA"
+ echo "$IP_PROPIA"
 
-echo "LSTP Client (Lechuga Speaker Transfer Protocol)"
+ WORKING_DIR="client"
 
-echo "1. SEND HEADER (Client: $IP_PROPIA, Server: $IP_SERVER)"
+ echo "LSTP Client (Lechuga Speaker Transfer Protocol)"
 
-echo "LSTP_1.1 $IP_PROPIA" | nc localhost $PORT
+ echo "1. SEND HEADER (Client: $IP_PROPIA, Server: $IP_SERVER)"
 
-echo "2. LISTEN OK_HEADER"
+ echo "LSTP_1.1" | nc 192.168.1.164 $PORT
 
-DATA=`nc -l $PORT`
+ echo "2. LISTEN OK_HEADER"
 
-if [ "$DATA" != "OK_HEADER" ]
-then
-	echo "Error de comunicacion con el servidor"
+ DATA=`nc -l $PORT`
 
-	exit 1
+ if [ "$DATA" != "OK_HEADER" ]
+ then
+     echo "Error de comunicacion con el servidor"
 
-fi
+     exit 1
 
-#cat client/lechuga1.lechu | text2wave -o client/lechuga1.wav
+ fi
 
-#yes | ffmpeg -i client/lechuga1.wav client/lechuga1.ogg
+ #cat client/lechuga1.lechu | text2wave -o client/lechuga1.wav
 
-echo "7. SEND FILE_NAME"
+ #yes | ffmpeg -i client/lechuga1.wav client/$FILE_NAME
 
-echo ""FILE_NAME" lechuga1.ogg" | nc $IP_SERVER $PORT
+ echo "7.1 SEND NUM_FILES"
 
-echo "8. LISTEN"
+ NUM_FILES=`ls client/*.lechu | wc -l`
 
-DATA=`nc -l $PORT`
+ echo "NUM_FILES $NUM_FILES" | nc $IP_SERVER $PORT
 
-if [ "$DATA" != "OK_FILE_NAME" ]
-then
-	echo "ERROR 2: FILE_NAME el archivo no se envio correctamente"
-	exit 2
-fi
+ echo "7.2 LISTEN OK_NUM_FILES"
 
-echo "12. SEND_FILE_DATA"
+ DATA=`nc -l $PORT`
 
-cat ./client/lechuga1.ogg | nc $IP_SERVER $PORT
+ if [ $DATA != "OK_NUM_FILES" ]
+ then
+     echo "ERROR 1.5: Error al recivir el OK"
+     exit 15
+ fi
+ echo "OK_NUM_FILES"
+ echo "7.3 SEND FILES"
 
-echo "13. LISTEN OK/KO_FILE_DATA"
+ for FILE_NAME in `ls $WORKING_DIR/*.lechu`
+ do
+ echo "7.4 SEND FILE_NAME"
 
-DATA=`nc -l $PORT`
+ FILE_NAME=`basename $FILE_NAME`
 
-if [ "$DATA" != "OK_FILE_DATA" ]
-then
-	echo "ERROR 3: Error al enviar los datos"
-	exit 3
-fi
+ echo "FILE_NAME $WORKING_DIR$FILE_NAME" | nc $IP_SERVER $PORT
 
-echo "16.SEND FILE_DATA_MD5"
+ echo "8. LISTEN"
 
-MD5=`cat ./client/lechuga1.ogg | md5sum | cut -d " " -f 1`
+ DATA=`nc -l $PORT`
 
-echo "FILE_DATA_MD5 $MD5" | nc $IP_SERVER $PORT
+ if [ "$DATA" != "OK_FILE_NAME" ]
+ then
+     echo "ERROR 2: FILE_NAME el archivo no se envio correctamente"
+     exit 2
+ fi
 
-echo "FiN"
+ echo "12. SEND_FILE_DATA"
 
-exit 0
+ cat $WORKING_DIR/$FILE_NAME | nc $IP_SERVER $PORT
+
+ echo "13. LISTEN OK/KO_FILE_DATA"
+
+ DATA=`nc -l $PORT`
+
+ if [ "$DATA" != "OK_FILE_DATA" ]
+ then
+     echo "ERROR 3: Error al enviar los datos"
+     exit 3
+ fi
+
+ echo "16.SEND FILE_DATA_MD5"
+
+ MD5=`cat $WORKING_DIR/$FILE_NAME | md5sum | cut -d " " -f 1`
+
+ echo "FILE_DATA_MD5 $MD5" | nc $IP_SERVER $PORT
+ #recivir OK y KO
+
+ echo "17. LISTEN FILE_DATA_MD5"
+ DATA=`nc -l $PORT`
+ if [ "$DATA" != "OK_FILE_DATA_MD5" ]
+ then
+     echo "ERROR 6: El archivo no pasa la validacion por MD5"
+     exit 6
+ fi
+ done
+ echo "FiN"
--- server.sh
+++ server.sh
@@ -1,87 +1,138 @@
-#!/bim/bash
+ #!/bim/bash
 
-PORT="7777"
+ PORT="7777"
 
-IP_CLIENT="localhost"
+ IP_CLIENT="192.168.1.164"
 
-echo "LSTP server (lechuga speaker transfer protocol)"
+ echo "LSTP server (lechuga speaker transfer protocol)"
 
-echo "0.Listen"
+ echo "0.Listen"
 
-$DATA
+ $DATA
 
-DATA=`nc -l $PORT`
+ DATA=`nc -l $PORT`
 
-HEADER=`echo "$DATA" | cut -d " " -f 1 `
+ HEADER=`echo "$DATA" | cut -d " " -f 1`
 
-echo "3. CHECK HEADER"
+ echo "$HEADER"
 
-if [ "$HEADER" != "LSTP_1.1" ]
-then
-	echo "ERROR 1: Header mal formado"
+ echo "3. CHECK HEADER"
 
-	echo "KO_HEADER" |nc $IP_CLIENT $PORT
+ if [ "$HEADER" != "LSTP_1.1" ]
+ then
+     echo "ERROR 1: Header mal formado"
 
-	exit 1
+     echo "KO_HEADER" |nc $IP_CLIENT $PORT
 
+     exit 1
+
+ fi
+
+ echo "4. SEND OK_HEADER"
+
+ echo "OK_HEADER" | nc $IP_CLIENT $PORT
+
+ echo "5.1 LISTEN NUM_FILES"
+
+ DATA=`nc -l $PORT`
+
+ echo "5.2 CHECK HEADER NUM_FILES"
+
+ PREFIX=`echo $DATA | cut -d " " -f 1`
+
+ if [ $PREFIX != "NUM_FILES" ]
+ then
+     echo "ERROR 22: El numero de archivos no cocuerda con los locales"
+     echo "KO_NUM_FILE" | nc $IP_CLIENT $PORT
+     exit 22
+ fi
+
+ echo "5.3 CHECK NUM_FILES"
+
+ NUM_FILES=`echo $DATA | cut -d " " -f 2`
+
+ CHECK_NUM_FILES=`echo "$NUM_FILES" | grep -E "^-?[0-9]+$"`
+
+ if [ $CHECK_NUM_FILES == " " ]
+ then
+     echo "ERROR 23: El numero de archivos es erroneo"
+     echo "KO_CHECK_NUM_FILE"
+     exit 23
 fi
+ echo "OK_NUM_FILES" | nc $IP_CLIENT $PORT
 
-echo "4. SEND OK_HEADER"
+ if [ $NUM_FILES != $CHECK_NUM_FILES ]
+ then
+     echo "ERROR X:No coincide el numero de archivos"
+     echo "KO_NUM_FILES"
+ fi
 
-echo "OK_HEADER" | nc $IP_CLIENT $PORT
+ for NUM in `seq $NUM_FILES`
+ do
 
-echo "5. LISTEN FILE_NAME"
+ echo "5.X LISTEN FILE_NAME $NUM"
 
-DATA=`nc -l $PORT`
+ DATA=`nc -l $PORT`
 
-PREFIX=`echo $DATA | cut -d " " -f 1`
+ PREFIX=`echo $DATA | cut -d " " -f 1`
 
-if [ "$PREFIX" != "FILE_NAME" ]
-then
-	echo "ERROR 2: FILE_NAME incorrecto"
-	echo "KO_FILE_NAME" | nc $IP_CLIENT $PORT
-	exit 2
-fi
+ if [ "$PREFIX" != "FILE_NAME" ]
+ then
+     echo "ERROR 2: FILE_NAME incorrecto"
+     echo "KO_FILE_NAME" | nc $IP_CLIENT $PORT
+     exit 2
+ fi
 
-FILE_NAME=`echo $DATA | cut -d " " -f 2`
+ FILE_NAME=`echo $DATA | cut -d " " -f 2`
 
-echo "10. SEND OK_FILE_NAME"
-echo "OK_FILE_NAME" | nc $IP_CLIENT $PORT
+ echo "10. SEND OK_FILE_NAME"
+ echo "OK_FILE_NAME" | nc $IP_CLIENT $PORT
 
-echo "11. LISTEN FILE DATA"
+ echo "11. LISTEN FILE DATA"
 
-nc -l $PORT > server/$FILE_NAME
+ nc -l $PORT > "server/$FILE_NAME"
 
-echo "14.SEND OK_FILE_DATA"
-DATA=`cat server/$FILE_NAME | wc -c`
+ echo "14.SEND OK_FILE_DATA"
+ DATA=`cat server/$FILE_NAME | wc -c`
 
-if [ $DATA -eq 0 ]
-then
-	echo "ERROR 3: Datos mal formados (vacios)"
- 	echo "KO_FILE_DATA" | NC $IP_CLIENT $PORT
- 	exit 3
-fi
-echo "OK_FILE_DATA" | nc $IP_CLIENT $PORT
+ if [ $DATA -eq 0 ]
+ then
+     echo "ERROR 3: Datos mal formados (vacios)"
+     echo "KO_FILE_DATA" | nc $IP_CLIENT $PORT
+     exit 3
+ fi
+ echo "OK_FILE_DATA" | nc $IP_CLIENT $PORT
 
-echo "15.LISTEN DATA_FILE_MD5"
+ echo "15.LISTEN DATA_FILE_MD5"
 
-DATA=`nc -l $PORT`
+ DATA=`nc -l $PORT`
 
-PREFIX=`echo $DATA | cut -d " " -f 1`
 
-if [ $PREFIX != "FILE_DATA_MD5" ]
-then
-	echo "KO_FILE_DATA_MD5"
-	echo "ERROR 4: Prefijo incorrecto"
-fi
 
-MD5=`cat ./server/lechuga1.ogg | md5sum | cut -d " " -f 1`
+ PREFIX=`echo $DATA | cut -d " " -f 1`
 
-if [ `echo $DATA | cut -d " " -f 2` != $MD5 ]
-then
-	echo "ERROR 5: Los datos han sido corrompidos/diferentes a los enviados"
-	echo "KO_FILE_MD5"
-fi
-echo "OK_FILE_DATA_MD5"
-echo "FIN"
-exit 0
+ if [ "$PREFIX" != "FILE_DATA_MD5" ]
+ then
+     echo "KO_FILE_DATA_MD5" | nc $IP_CLIENT $PORT
+     echo "ERROR 4: Prefijo incorrecto"
+     exit 4
+ fi
+
+ MD5=`cat ./server/*.lechu | md5sum | cut -d " " -f 1`
+ RECIVED_MD5=`echo $DATA | cut -d " " -f 2`
+ echo "$MD5"
+ echo "$RECIVED_MD5"
+
+ if [ "$RECIVED_MD5" != "$MD5" ]
+ then
+     echo "ERROR 5: Los datos han sido corrompidos/diferentes a los enviados"
+     echo "KO_FILE_MD5" | nc $IP_CLIENT $PORT
+     exit 5
+ fi
+
+ echo "OK_FILE_DATA_MD5" | nc $IP_CLIENT $PORT
+
+ done
+
+ echo "FIN"
+ exit 0
